08f74b8a09926cbad854e88187dd3aa031f6ef46
sairam1103 <65078740+sairam1103@users.noreply.github.com>
2020-12-15 10:42:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,8 +12,6 @@ import os
 
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
-    from sample_config import Config
-else:
     from config import Config
 
 import pyrogram
